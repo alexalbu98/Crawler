807efcae34edc0645fa9e1f3236384127578371c
Teo Calugaru <calugaruteodor@yahoo.com>
2020-12-04 19:19:21 +0200

    Creare functie de citire fisier de configurare Issue #10 #14 Implementare functie prin care se citesc informatii de care avem nevoie ulterior din fisierul de configurare Signed-off-by: Teo Calugaru <calugaruteodor@yahoo.com>

--- src/com/mta/Crawler.java
+++ src/com/mta/Crawler.java
@@ -1,5 +1,7 @@
 package com.mta;
 
+import java.io.*;
+
 public class Crawler {
     private String site_file;
     private String config_file;
@@ -14,10 +16,33 @@ public class Crawler {
     public void checkArgs(){
 
     }
-    public void readConfigFile(String conf_filename){
-
+    public void readConfigFile() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(config_file));
+        String st, parts[];
+        while ((st = br.readLine()) != null) {
+            parts = st.split("=");
+            switch (parts[0]) {
+                case "n_threads":
+                    nThreads = Integer.parseInt(parts[1]);
+                    break;
+                case "depth":
+                    depth = Integer.parseInt(parts[1]);
+                    break;
+                case "log_level":
+                    log_level = Integer.parseInt(parts[1]);
+                    break;
+                case "root_dir":
+                    root_dir = parts[1];
+                    break;
+                case "delay":
+                    delay = Integer.parseInt(parts[1].substring(0, parts[1].length() - 2));
+                    break;
+            }
+        }
+        String print="n_threads:"+nThreads+"\ndepth:"+depth+"\nlog_level:"+log_level+"\nroot_dir:"+root_dir+"\ndelay:"+delay;
+        System.out.println(print);
     }
-    public void readSitesFile(String sites_filename){
+    public void readSitesFile(){
 
     }
     public void createThreadPool() {
--- src/com/mta/Main.java
+++ src/com/mta/Main.java
@@ -1,8 +1,13 @@
 package com.mta;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+	//System.out.println("Hello world!");
+	Crawler C=new Crawler();
+	C.readConfigFile();
+
     }
 }

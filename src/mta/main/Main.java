07e14a68dca9957522ee1bc7ebab98f8e569ae23
Albu Alexandru <alexalbu78@gmail.com>
2020-12-05 14:15:37 +0200

    Reparare structura main
    Issue: #10 #14 #11
    
    Repararea structurii proiectului de pe master.
    
    Signed-off-by: Albu Alexandru <alexalbu78@gmail.com>

--- src/mta/main/Main.java
+++ src/mta/main/Main.java
@@ -1,16 +1,9 @@
 package mta.main;
-<<<<<<< HEAD:src/mta/main/Main.java
-=======
-
 import mta.Classes.Crawler;
->>>>>>> db62977 (Modificare structura):src/com/mta/Main.java
-
 import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	//System.out.println("Hello world!");
 	Crawler C=new Crawler();
 	C.readConfigFile();
 

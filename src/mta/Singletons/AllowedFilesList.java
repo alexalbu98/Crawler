661cb7c9ff3e91a0153538d73b1459ca2588047c
Albu Alexandru <alexalbu78@gmail.com>
2020-12-05 11:36:27 +0200

    Creare clase Singleton
    Issue: fixes #9
    
    Prin acest commit sunt implementate toate clasele singleton care vor fi
    folosit in proiect.
    
    Signed-off-by: Albu Alexandru <alexalbu78@gmail.com>

---
+++ src/mta/Singletons/AllowedFilesList.java
@@ -0,0 +1,33 @@
+package mta.Singletons;
+import java.util.ArrayList;
+
+public class AllowedFilesList {
+    private static AllowedFilesList instance = null;
+    private ArrayList<String> types;
+
+    private AllowedFilesList()
+    {
+        types = new ArrayList<String>();
+    }
+
+    static public AllowedFilesList getInstance()
+    {
+        if(instance == null){
+            instance = new AllowedFilesList();
+        }
+        return instance;
+    }
+
+   public boolean isTypeAllowed(String type)
+   {
+       if(types.contains(type)){
+           return false;
+       }
+       return true;
+   }
+
+   public void addType(String type)
+   {
+       types.add(type);
+   }
+}

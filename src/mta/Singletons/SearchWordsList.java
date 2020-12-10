09dbf55e0018eda8ee436c169d70e5612db0bae9
Teo Calugaru <calugaruteodor@yahoo.com>
2020-12-10 22:09:12 +0200

    Creare clasa SearchWordsTask Issue #28 Se creeaza clasa SearchWordsTask care permite cautarea de cuvinte cheie in fisierele descarcate
    Signed-off-by: Teo Calugaru <calugaruteodor@yahoo.com>

---
+++ src/mta/Singletons/SearchWordsList.java
@@ -0,0 +1,65 @@
+package mta.Singletons;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import mta.Classes.Page;
+
+/** This class stores a map that contains the key words given by user and the files
+ * where these words can be found. */
+public class SearchWordsList<Map> {
+
+    private static SearchWordsList instance = null;
+    HashMap<String, ArrayList<String>> words_map;
+
+
+    private SearchWordsList()
+    {
+        words_map = new HashMap<String,ArrayList<String>>();
+    }
+
+    static public SearchWordsList getInstance()
+    {
+        if(instance == null){
+            instance = new SearchWordsList();
+        }
+        return instance;
+    }
+
+    public HashMap<String,ArrayList<String>> getWordsMap()
+    {
+        return this.words_map;
+    }
+
+    public void addWords(String word,String path)
+    {
+        ArrayList<String> list;
+        if(this.words_map.get(word)==null){
+            list=new ArrayList<String>();
+        }
+        else{
+            list=this.words_map.get(word);
+        }
+        list.add(path);
+        this.words_map.put(word,list);
+    }
+    public void print() throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("searched_words.txt", "UTF-8");
+        for (java.util.Map.Entry<String,ArrayList<String>> entry : this.words_map.entrySet()) {
+            String key = entry.getKey();
+            ArrayList<String> value = entry.getValue();
+            writer.println("-----------------------");
+            String output="Key word \""+key+"\" was found in the next files:";
+            writer.println(output);
+            for (String i:value){
+                output="     -"+i;
+                writer.println(output);
+            }
+        }
+        writer.close();
+    }
+
+}
--- src/mta/main/Main.java
+++ src/mta/main/Main.java
@@ -1,12 +1,14 @@
 package mta.main;
 import mta.Classes.Crawler;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedURLException, InterruptedException, ExecutionException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException, ExecutionException, FileNotFoundException, UnsupportedEncodingException {
         Crawler crawler = new Crawler(args);
         crawler.runCrawler();
     }

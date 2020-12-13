cbd08f16d0172cf9f6a2407a3b9e0c04d3cbff37
Ira Mirea <iramadd@gmail.com>
2020-12-13 23:30:25 +0200

    Creare Filter Task Issue: fixes #30 am creat clasa filter task pentru filtrarea fisierelor in functie de tip
    Signed-off-by: Ira Mirea <iramadd@gmail.com>

--- src/mta/Classes/FilterTask.java
+++ src/mta/Classes/FilterTask.java
@@ -2,7 +2,22 @@ package mta.Classes;
 
 public class FilterTask implements Runnable {
     @Override
-    public void run() {
+    public void run() 
+    {
+        AllowedFilesList allowed_files=AllowedFilesList.getInstance();
+        String[] pathnames;
+        File f = new File(root_dir);
+        pathnames = f.list();
 
-    }
+         for (String pathname : pathnames) {
+               if(AllowedFilesList.isTypeAllowed(pathname)==0)
+                  {
+                    File filter = new File(pathname);
+                    filter.delete();
+                    filter.close();
+                  } 
+          }
+     }
 }
+
+

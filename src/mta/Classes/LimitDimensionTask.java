154d335bcbaa571fbca7925d580fa2e1dd203782
Cristotovici <cristian.ghita3@yahoo.com>
2020-12-13 23:58:11 +0200

    Creare Clasa LimitDimensionTask Issue: fixes #29 Crearea clasei LimitDimensionTask si introducerea unei metode simple de sortare a fisierelor
    Signed-off-by: Cristotovici <cristian.ghita3@yahoo.com>

--- src/mta/Classes/LimitDimensionTask.java
+++ src/mta/Classes/LimitDimensionTask.java
@@ -1,8 +1,30 @@
 package mta.Classes;
+import java.io.*;
+import java.util.*;
 
 public class LimitDimensionTask implements Runnable {
-    @Override
-    public void run() {
 
+    private String root_dir;
+    public LimitDimensionTask(String dir)
+    {
+        root_dir=dir;
+    }
+    public void run() {
+        File dir = new File(root_dir);
+
+        File[] files = dir.listFiles();
+
+        Arrays.sort(files, (f1, f2) -> {
+            return new Long(f1.length()).compareTo(new Long(f2.length()));
+        });
+
+        for (File file : files) {
+            if (!file.isHidden()) {
+                if (!file.isDirectory()) {
+                    System.out.println("FILE\t" + " " + file.length() +
+                            " bytes\t\t" + file.getName());
+                }
+            }
+        }
     }
 }

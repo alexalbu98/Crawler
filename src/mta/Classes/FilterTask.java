fce2a1fe73c8f13e534e0e2c9d7a0473d59d3063
Ira Mirea <iramadd@gmail.com>
2020-12-21 00:22:55 +0200

    my last updates issue: fixes #32, fixes #47
    Signed-off-by: Ira Mirea <iramadd@gmail.com>

--- src/mta/Classes/FilterTask.java
+++ src/mta/Classes/FilterTask.java
@@ -8,27 +8,34 @@ import java.util.Scanner;
 
 public class FilterTask implements Runnable {
     private String root_dir;
-   public FilterTask(String dir)
-    {
-        root_dir=dir;
+
+    public FilterTask(String dir) {
+        root_dir = dir;
     }
-    @Override
-    public void run() 
-    {
-        AllowedFilesList allowed_files=AllowedFilesList.getInstance();
-        String[] pathnames;
-        File f = new File(root_dir);
-        pathnames = f.list();
 
-         for (String pathname : pathnames) {
-               if(allowed_files.isTypeAllowed(pathname)==0)
-                  {
-                    File filter = new File(pathname);
-                    filter.delete();
+    public void run(String dir) {
+        AllowedFilesList allowed_files = AllowedFilesList.getInstance();
+        try {
+            File folder = new File(dir);
+            boolean chk;
+            File[] listOfFiles = folder.listFiles();
+            for (File file : listOfFiles) {
+                if (file.isFile()) {
 
-                  } 
-          }
-     }
-
+                    if (allowed_files.isTypeAllowed(file) == 0) {
+                        System.out.print("Found file to delete");
+                        chk = file.delete();
+                        if (chk) {
+                            System.out.print("File succesfully deleted");
+                        }
+                    }
+                }
+                if (file.isDirectory()) {
+                    run(file.getPath());
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
-
